b2354ce9482b65c5aa83a59cf06cd7b36e469355
IRod22 <ivan18rod@yahoo.com>
2022-12-17 22:43:46 -0600

    docs: remove demos

--- demos/btn-demo/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { e, t, on } from '../../../src/index'
-import mount from '../../../src/client/index'
-
-const app = e('button', [on('click', () => () => alert('Hello, NimbleUI!'))], [t('Click here')])
-
-window.addEventListener('load', function() {
-    mount(app, this.document.body)
-})
