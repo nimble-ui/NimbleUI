ff0d0fa8f5938c1d1bd14af40fe0a5f3ff233e6b
IRod22 <ivan18rod@yahoo.com>
2022-12-19 16:16:38 -0600

    docs: document types and client API

--- src/internal/client/index.ts
+++ src/internal/client/index.ts
@@ -2,6 +2,12 @@ import { setChildren, INode } from './manipulation'
 import { render } from './render'
 import type { Render } from '../shared/types'
 
+/**
+ * Mounts the application
+ * @param template the template to render to `root`
+ * @param root the root element to render `template` to
+ * @returns an object to update and shut down the view.
+ */
 export default function mount(template: Render, root: HTMLElement) {
     let rerender = () => {}, children: INode[] = []
     const rendered = render(template, [], () => rerender())
